b9fb57036c35b14bcb53036a27e654a8abdd60e1
Mikhail <mikhailj.lajom@gmail.com>
2022-08-18 16:08:55 -0700

    completed intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,9 @@
 import "./intro.scss"
+import Typewriter from 'typewriter-effect';
 
 export default function Intro() {
+
+
   return (
     <div className='intro' id="intro">
       <div className="left">
@@ -12,10 +15,23 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm </h2>
           <h1>Mikhail J. Abaya Lajom</h1>
-          <h3>Software Engineer <span></span></h3>
+          <h3>Software Engineer
+            <span>
+              <Typewriter
+
+                options={{
+                  strings: ["Full-Stack", "React.Js", "Node.Js"],
+                  autoStart: true,
+                  delay: 75,
+                  loop: true
+                }}
+
+              />
+            </span>
+          </h3>
         </div>
         <a href="#portfolio">
-        <img src="assets/down-arrow.png" alt=""/>
+          <img src="assets/down-arrow.png" alt="" />
         </a>
       </div>
     </div>
