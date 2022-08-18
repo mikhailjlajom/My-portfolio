2942c7fee5b0725c57b1e39c45e6286de0268df6
Mikhail <mikhailj.lajom@gmail.com>
2022-08-18 15:31:50 -0700

    added images and intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,22 @@ import "./intro.scss"
 
 export default function Intro() {
   return (
-    <div className='intro' id="intro"></div>
+    <div className='intro' id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/assets/betterme.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm </h2>
+          <h1>Mikhail J. Abaya Lajom</h1>
+          <h3>Software Engineer <span></span></h3>
+        </div>
+        <a href="#portfolio">
+        <img src="assets/down-arrow.png" alt=""/>
+        </a>
+      </div>
+    </div>
   )
 }
