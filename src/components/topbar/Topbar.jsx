fcdf910cc347265e9a8bd3370e49a081a7f5c3b1
Mikhail <mikhailj.lajom@gmail.com>
2022-08-18 12:53:16 -0700

    completed topbar with transition

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import "./topbar.scss"
 import PersonIcon from '@mui/icons-material/Person';
 import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Mikhail.</a>
@@ -18,6 +18,11 @@ export default function Topbar() {
             </div>
         </div>
         <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
